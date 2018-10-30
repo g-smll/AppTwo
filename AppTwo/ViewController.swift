320a9c49d11e834a3056d4e556f77fb93771ce05
chengang <chengangyunnan@qq.com>
2018-10-30 13:34:19 +0800

    Initial Commit

---
+++ AppTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTwo
+//
+//  Created by 陈纲 on 2018/10/30.
+//  Copyright © 2018 陈纲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
